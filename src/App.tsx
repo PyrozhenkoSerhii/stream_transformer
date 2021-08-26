14fb51cc256d4684b38b39c94e248cba014e0cfa
PyrozhenkoSerhii <serhii.pyrozhenko@nure.ua>
2021-08-26 21:59:13 +0300

    setup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { WatermarkTransformerComponent } from "./WatermarkTransformer/watermarkTransformer.component";
+
+export const App = (): JSX.Element => {
+  return (
+    <WatermarkTransformerComponent />
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+
+import { App } from "./App";
+
+const GlobalStyle = createGlobalStyle`
+  html, body, #root {
+    margin: 0;
+    min-height: 100%;
+    overflow-y: hidden;
+    display: flex;
+    flex-grow: 1;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+render(
+  <BrowserRouter>
+    <GlobalStyle />
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root"),
+);
